b26a16fdb185a9bdfcb2d60c5c0d697777aa0efb
Tracy Ogutu <tracyogutu@gmail.com>
2019-11-18 11:01:06 +0300

    change title

--- app/main/views.py
+++ app/main/views.py
@@ -1,69 +1,37 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..request import get_article,get_source
-from .forms import ReviewForm
-from ..model import Review
+from ..request import get_source,get_articles
+from ..model import Source
 
-# Review = reviews.Review
-
-#views
-# @main.route('/')
-# def index():
     
 @main.route('/') #route decorator
 def index():#index() is the view function
 
+    # entertainment_news = get_source('entertainment')
+    # print(entertainment_news)
+    # general_news = get_source('general')
+    # sports_news = get_source('sports')
+    # science_news = get_source('science')
+    # technology_news = get_source('technology')
 
-   #getting popular movies
-    popular_movies  = get_movies('popular')
-    # print(popular_movies)
-    upcoming_movies = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
+    general_news = get_source('general')
+    print('*************general news*********************')
+    print(general_news)
 
-    title='Home - Welcome to The Best Movie Review Online'
+    title='Latest News'
 
-    search_movie = request.args.get('movie_query')
+    return render_template('index.html',title = title,general=general_news)
+    # , entertainment = entertainment_news, general = general_news, sports = sports_news, 
+    # science = science_news, technology = technology_news)
 
-    if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+@main.route('/article/<int:id>')
+def article(id):
 
-    else:
-        return render_template('index.html',title = title, popular = popular_movies,upcoming = upcoming_movies,now_showing = now_showing_movie) #response
+    article = get_articles(id)
+    print('*************get_article****************')
+    print(article)
 
-@main.route('/movie/<int:id>')
-def movie(id):
+    title = f'{id}'
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    movie = get_movie(id)
-    title = f'{movie.title}'
-    reviews = Review.get_reviews(movie.id)
-
-    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-    
-@main.route('/search/<movie_name>')
-def search(movie_name):
-
-    movie_name_list=movie_name.split(" ")
-    movie_name_format= "+".join(movie_name_list)
-    searched_movies=search_movie(movie_name_format)
-    title=f'search results for {movie_name}'
-    return render_template('search.html',movies=searched_movies)
-
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-def new_review(id):
-    form = ReviewForm()
-    movie = get_movie(id)
-
-    if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
-        new_review = Review(movie.id,title,movie.poster,review)
-        new_review.save_review()
-        return redirect(url_for('main.movie',id = movie.id ))
-
-    title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
-
+    return render_template('trial.html',title = title,article = article)
 
